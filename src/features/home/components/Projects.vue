a747789980aa01f3af8b599361772dfe2dfece15
JepongDevxyz <businessdevgithubjepong@gmail.com>
2026-09-25 23:01:12 +0800

    content: hide projects section until portfolio projects are added

--- src/features/home/components/Projects.vue
+++ src/features/home/components/Projects.vue
@@ -6,7 +6,6 @@ import PreviewCard from "../../projects/components/PreviewCard.vue";
 import NotchSection from "../../../components/NotchSection.vue";
 import Banner from "../../../components/Banner.vue";
 import { t } from "../../../i18n/utils/translate";
-import { isFeatureEnabled } from "../../../utils/features";
 
 import type { ProjectPreview } from "../../../content/types";
 
@@ -31,7 +30,7 @@ onMounted(loadPreviews);
 </script>
 
 <template>
-  <div class="projects">
+  <div v-if="loadedPreviews?.length" class="projects">
     <NotchSection class="projects-notch-start" />
     <NotchSection class="projects-notch-end" />
     <div class="grid">
@@ -43,7 +42,6 @@ onMounted(loadPreviews);
     <div class="grid">
       <div class="projects-cards">
         <PreviewCard v-for="preview in loadedPreviews" :key="preview.title" :preview="preview" />
-        <PreviewCard v-if="isFeatureEnabled('startProject')" />
       </div>
     </div>
   </div>
