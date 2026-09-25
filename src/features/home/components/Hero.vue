ff64baf35c3c488b34ae708799e01d2ddce97490
JepongDevxyz <businessdevgithubjepong@gmail.com>
2026-09-25 22:33:01 +0800

    Update hero title in Hero.vue component

--- src/features/home/components/Hero.vue
+++ src/features/home/components/Hero.vue
@@ -11,7 +11,7 @@ import AppearingText from "../../../components/AppearingText.vue";
     <div class="hero-content grid">
       <div class="hero-content-inner" id="hero-content-inner">
         <div class="hero-content-copys">
-          <h1 class="hero-title">Your<br />Name</h1>
+          <h1 class="hero-title">Jepong<br />Devxyz</h1>
           <Banner class="hero-banner" :copy="t('job-title')" v-if="!preloaderVisible" animated />
         </div>
       </div>
